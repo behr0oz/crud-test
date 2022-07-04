73786ae64185178525f1a174f0ade30668f02c3d
Behrooz <b.sohrabloo@gmail.com>
2022-07-04 17:13:31 +0430

    Created Domain Entity

---
+++ Services/Customer/Customer.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; protected set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? LastModifiedDate { get; set; }
+    }
+}
---
+++ Services/Customer/Customer.Domain/Entities/CustomerModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Domain.Entities
+{
+    public class CustomerModel
+    {
+        [Column(TypeName = "varchar(100)")]
+        public string Firstname { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Lastname { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Column(TypeName = "varchar(15)")]
+        public string PhoneNumber { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string BankAccountNumber { get; set; }
+    }
+}

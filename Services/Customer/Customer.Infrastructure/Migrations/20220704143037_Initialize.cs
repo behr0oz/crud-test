4ae1980e8e8271efbf127c1cc09196e6a22e6b6b
Behrooz <b.sohrabloo@gmail.com>
2022-07-04 19:00:53 +0430

    updated EF migration

---
+++ Services/Customer/Customer.Infrastructure/Migrations/20220704143037_Initialize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Customer.Infrastructure.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Lastname = table.Column<string>(type: "varchar(100)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "date", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(15)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    BankAccountNumber = table.Column<string>(type: "varchar(20)", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}

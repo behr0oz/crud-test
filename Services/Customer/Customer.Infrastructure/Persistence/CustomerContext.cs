7b5edd33e3d26ba23cfe9c3ce4696d17cdb24edf
Behrooz <b.sohrabloo@gmail.com>
2022-07-04 17:46:14 +0430

    1.Created infrastructure structure 2. Implemented Customer interface

---
+++ Services/Customer/Customer.Infrastructure/Persistence/CustomerContext.cs
@@ -0,0 +1,19 @@
+﻿using Customer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Infrastructure.Persistence
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
---
+++ Services/Customer/Customer.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Customer.Application.Contracts.Persistence;
+using Customer.Domain.Entities;
+using Customer.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Infrastructure.Repositories
+{
+    public class CustomerRepository : RepositoryBase<CustomerModel>, ICustomerRepository
+    {
+        public CustomerRepository(CustomerContext dbContext) : base(dbContext)
+        {
+
+        }
+
+
+    }
+}
